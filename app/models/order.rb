b50e06e1b00c22ce025f8d7d4b2c605f08ae4ef5
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-03-28 14:50:06 -0400

    fix order_count

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   has_many :cartitems
-  belongs_to :customer, counter_cache: true
+  belongs_to :customer
   has_one :delivery
 
   validates :customer, presence: true
