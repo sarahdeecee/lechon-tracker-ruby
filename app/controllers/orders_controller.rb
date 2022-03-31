78b759ae6d5c325a662da452408acad2d9bbe5fe
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-03-31 17:35:03 -0400

    implement order form for simple data

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,17 +25,54 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.new(order_params)
-
     if @order.save
-      redirect_to @order
+      redirect_to '/orders', notice: 'Account registered!'
     else
-      render :new
+      redirect_to '/orders', notice: 'Failed'
     end
+
+    # if order.valid?
+    #   empty_cart!
+    #   redirect_to order, notice: 'Your Order has been placed.'
+    # else
+    #   redirect_to cart_path, flash: { error: order.errors.full_messages.first }
+    # end
   end
 
   def order_params
-    params.require(:order).permit(
-      :first_name, :last_name, :pickup_at
+    params.require(:order).permit(:pickup_at, :notes, :customer_id)
+    params.permit(:customer_id)
+  end
+
+  def empty_cart!
+    # empty hash means no products in cart :)
+    update_cart({})
+  end
+
+  def create_order
+    order = Order.new(
+      customer_id: :id,
+      confirmed_at: DateTime.now,
+      pickup_at: :pickup_at,
+      total_price: cart_subtotal_cents,
+      notes: :notes,
     )
+    puts order
+    # delivery
+
+    # lechon_id
+
+    # enhanced_cart.each do |entry|
+    #   product = entry[:product]
+    #   quantity = entry[:quantity]
+    #   order.cart_items.new(
+    #     product: product,
+    #     quantity: quantity,
+    #     item_price: product.price,
+    #     total_price: product.price * quantity
+    #   )
+    # end
+    order.save!
+    order
   end
 end
