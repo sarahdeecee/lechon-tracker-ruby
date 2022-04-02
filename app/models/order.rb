509e13a4f1cc8008a4ac8ec9a7c8829af658368f
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-04-01 23:12:38 -0400

    implement order price

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,7 +7,10 @@ class Order < ApplicationRecord
 
   validates :customer, presence: true
 
-  def total_price
-    50
+  def total_sell_price
+    Lechon.where(order_id: self.id).sum { |lechon| lechon.sell_price }
+  end
+  def total_base_price
+    Lechon.where(order_id: self.id).sum { |lechon| lechon.base_price }
   end
 end
