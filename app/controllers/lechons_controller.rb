0dc856caa4d4947c9db77dffac241f43a585c21c
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-04-02 17:49:05 -0400

    implement delete lechon

--- app/controllers/lechons_controller.rb
+++ app/controllers/lechons_controller.rb
@@ -2,4 +2,15 @@ class LechonsController < ApplicationController
   def index
     @lechons = Lechon.all
   end
+
+  def show
+    @lechon = Lechon.find(params[:id])
+  end
+
+  def destroy
+    @lechon = Lechon.find(params[:id])
+    @lechon.destroy
+
+    redirect_to '/lechons', status: :see_other
+  end
 end
