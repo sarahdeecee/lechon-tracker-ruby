d9b3ee218a3777ae01de51953ff032270f673a4f
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-04-01 20:16:23 -0400

    implement cancel delivery on order edit page

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -9,4 +9,11 @@ class DeliveriesController < ApplicationController
 
   def new
   end
+
+  def destroy
+    @delivery = Delivery.find_by(order_id: params[:order_id])
+    @delivery.destroy
+
+    redirect_to '/orders', status: :see_other
+  end
 end
